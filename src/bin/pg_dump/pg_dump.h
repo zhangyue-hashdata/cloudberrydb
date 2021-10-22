0ee0531c30cc9ce34daaa2cca643cd9ba94c3af6
Tom Lane <tgl@sss.pgh.pa.us>
2021-10-22 17:19:03 -0400

    In pg_dump, use simplehash.h to look up dumpable objects by OID.
    Backported from PG15 92316a4582a5714d4e494aaf90360860e7fec37a
    
    Conflicts resolved:
    * Move GPDB specific numTypeStorageOptions to
      getSchemaData function
    * Refactor getTypeStorageOptions query to return
      tableoid and oid fields, which are required to
      correctly index the hash table
    
    Original commit message follows:
    
    Create a hash table that indexes dumpable objects by CatalogId
    (that is, catalog OID + object OID).  Use this to replace the
    former catalogIdMap array, as well as various other single-
    catalog index arrays, and also the extension membership map.
    
    In principle this should be faster for databases with many objects,
    since lookups are now O(1) not O(log N).  However, it seems that these
    lookups are pretty much negligible in context, so that no overall
    performance change can be measured.  But having only one lookup
    data structure to maintain makes the code simpler and more flexible,
    so let's do it anyway.
    
    Discussion: https://postgr.es/m/2595220.1634855245@sss.pgh.pa.us

--- src/bin/pg_dump/pg_dump.h
+++ src/bin/pg_dump/pg_dump.h
@@ -704,17 +704,6 @@ typedef struct _SubscriptionInfo
 } SubscriptionInfo;
 
 /*
- * We build an array of these with an entry for each object that is an
- * extension member according to pg_depend.
- */
-typedef struct _extensionMemberId
-{
-	CatalogId	catId;			/* tableoid+oid of some member object */
-	ExtensionInfo *ext;			/* owning extension */
-} ExtensionMemberId;
-
-extern const char *EXT_PARTITION_NAME_POSTFIX;
-/*
  *	common utility functions
  */
 
@@ -739,7 +728,7 @@ extern NamespaceInfo *findNamespaceByOid(Oid oid);
 extern ExtensionInfo *findExtensionByOid(Oid oid);
 extern PublicationInfo *findPublicationByOid(Oid oid);
 
-extern void setExtensionMembership(ExtensionMemberId *extmems, int nextmems);
+extern void recordExtensionMembership(CatalogId catId, ExtensionInfo *ext);
 extern ExtensionInfo *findOwningExtension(CatalogId catalogId);
 
 extern void parseOidArray(const char *str, Oid *array, int arraysize);
