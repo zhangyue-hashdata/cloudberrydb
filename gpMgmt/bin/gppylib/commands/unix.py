ace5385c486fc0b7a32b8d89d0a2eeb098e818e4
Nihal Jain <jnihal@vmware.com>
2022-09-14 14:56:07 +0530

    Replace netstat with ss (code cleanup)
    netstat has been replaced with ss earlier but some of the variable names and commented out codes were left as it is. This commit cleans up the old mentions of netstat and changes the variable names to avoid any confusion in the future.

--- gpMgmt/bin/gppylib/commands/unix.py
+++ gpMgmt/bin/gppylib/commands/unix.py
@@ -625,8 +625,8 @@ class PgPortIsActive(Command):
 
         for r in rows:
             val = r.split('.')
-            netstatport = int(val[len(val) - 1])
-            if netstatport == self.port:
+            ssport = int(val[len(val) - 1])
+            if ssport == self.port:
                 return True
 
         return False
