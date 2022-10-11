1e84aadd5eecc4b9f6b6bada24a8f2281342afaa
Chris Hajas <chajas@vmware.com>
2022-10-10 17:09:42 -0700

    Use external table FDW in Orca instead of manually constructing external table
    Previously, we added separate logic and replicated some of what was in
    the external table FDW since Orca did not support native FDWs. Since we
    now support foreign tables, this separate logic/code path is no longer
    needed.
    
    This was also a good test case for Foreign scan support in Orca

--- src/backend/gpopt/gpdbwrappers.cpp
+++ src/backend/gpopt/gpdbwrappers.cpp
@@ -1807,17 +1807,6 @@ gpdb::HasSubclassSlow(Oid rel_oid)
 	return false;
 }
 
-bool
-gpdb::RelIsExternalTable(Oid relid)
-{
-	GP_WRAP_START;
-	{
-		return rel_is_external_table(relid);
-	}
-	GP_WRAP_END;
-	return false;
-}
-
 GpPolicy *
 gpdb::GetDistributionPolicy(Relation rel)
 {
@@ -1901,19 +1890,6 @@ gpdb::GetRelation(Oid rel_oid)
 		return RelationWrapper{RelationIdGetRelation(rel_oid)};
 	}
 	GP_WRAP_END;
-}
-
-ForeignScan *
-gpdb::CreateForeignScanForExternalTable(Oid rel_oid, Index scanrelid,
-										List *qual, List *targetlist)
-{
-	GP_WRAP_START;
-	{
-		return BuildForeignScanForExternalTable(rel_oid, scanrelid, qual,
-												targetlist);
-	}
-	GP_WRAP_END;
-	return nullptr;
 }
 
 ForeignScan *
