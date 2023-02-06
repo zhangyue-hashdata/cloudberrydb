574537722ddaf8d5fc5d7eba891764b41204796b
Chris Hajas <chajas@vmware.com>
2023-02-06 11:55:22 -0800

    Fall back in Orca for queries with RTE of type TableFunc (#14898)
    Previously, Orca would crash if encountering this. For now, fall back
    similarly to what we do for RTE_TABLEFUNCTION.
    
    Example query: explain SELECT * FROM xmltable('/root' passing '' COLUMNS element text);
    
    This actually fell back correctly in retail build, but we were hitting a
    `__builtin_unreachable` call, which is undefined behavior, so we were
    likely just getting lucky.
    
    Fixes issue reported in https://groups.google.com/a/greenplum.org/g/gpdb-dev/c/4F8GUSs04L8

--- src/backend/gpopt/translate/CTranslatorQueryToDXL.cpp
+++ src/backend/gpopt/translate/CTranslatorQueryToDXL.cpp
@@ -3342,6 +3342,7 @@ CTranslatorQueryToDXL::UnsupportedRTEKind(RTEKind rtekind)
 					   GPOS_WSZ_LIT("RangeTableEntry of type Void"));
 		}
 		case RTE_TABLEFUNCTION:
+		case RTE_TABLEFUNC:
 		{
 			GPOS_RAISE(gpdxl::ExmaDXL, gpdxl::ExmiQuery2DXLUnsupportedFeature,
 					   GPOS_WSZ_LIT("RangeTableEntry of type Table Function"));
