a9385c1700fa4aebec0201e5ba5debe423e5db34
Brent Doil <bdoil@vmware.com>
2023-02-08 16:23:02 -0500

    Pass tableName to AppendOnlyStorageRead_ReadNextBlock_success
    Allows for targeted testing of blocks read for specific AO tables.
    
    Authored-by: Brent Doil <bdoil@vmware.com>

--- src/backend/cdb/cdbappendonlystorageread.c
+++ src/backend/cdb/cdbappendonlystorageread.c
@@ -27,6 +27,7 @@
 #include "cdb/cdbappendonlystorageformat.h"
 #include "cdb/cdbappendonlystorageread.h"
 #include "storage/gp_compress.h"
+#include "utils/faultinjector.h"
 #include "utils/guc.h"
 
 
@@ -1010,7 +1011,9 @@ AppendOnlyStorageRead_ReadNextBlock(AppendOnlyStorageRead *storageRead)
 	{
 		/* UNDONE: Finish the read for the information only header. */
 	}
-
+#ifdef FAULT_INJECTOR
+	FaultInjector_InjectFaultIfSet("AppendOnlyStorageRead_ReadNextBlock_success", DDLNotSpecified, "", storageRead->relationName);
+#endif
 	return true;
 }
 
