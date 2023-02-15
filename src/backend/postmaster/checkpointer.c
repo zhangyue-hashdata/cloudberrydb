170a45f75ef43411b5ff76bcc2c8fdc4f52a9a35
Andrey Sokolov <a.sokolov@arenadata.io>
2023-02-15 18:27:23 +0300

    Fix mirror checkpointer error on the alter database query
    Steps to reproduce:
    1. Set the fsync GUC to on.
    2. Make changes to a database and move it to another tablespace, for example:
    psql -c "create tablespace ts1 location '/tmp/ts'" postgres
    psql -c "create database test" postgres
    psql -c "create table t (i int)  DISTRIBUTED randomly" test
    psql -c "alter database test SET TABLESPACE ts1" postgres
    
    If a mirror checkpointer doesn't create checkpoint between "create table" and
    "alter database", then attempt to create checkpoint the next time will result in
    the file synchronization error, because the file doesn't exist. At the moment of
    checkpoint creating the database file has already been moved to a new
    tablespace, but fsync request to checkpointer was not changed.
    
    The error doesn't manifest itself in PostgreSQL. PostgreSQL removes files from
    the old tablespace and adds XLOG_DBASE_DROP record to WAL at the end of
    movedb(). GPDB has different implementation of movedb(). It schedules removing
    database directory on transaction commit. When PostgreSQL replica deletes old
    files, it processes the XLOG_DBASE_DROP record and calls the
    ForgetDatabaseSyncRequests() function. When GPDB mirror deletes old files, it
    processes XLOG_XACT_COMMIT_PREPARED and doesn't change sync requests.
    
    In the end of copydir() function the database is synced, so it is necessary to
    cancel all sync requests to checkpointer related to this database before the
    database files will be deleted.

--- src/backend/postmaster/checkpointer.c
+++ src/backend/postmaster/checkpointer.c
@@ -58,6 +58,7 @@
 #include "storage/shmem.h"
 #include "storage/smgr.h"
 #include "storage/spin.h"
+#include "utils/faultinjector.h"
 #include "utils/guc.h"
 #include "utils/memutils.h"
 #include "utils/resowner.h"
@@ -347,6 +348,11 @@ CheckpointerMain(void)
 		/* Clear any already-pending wakeups */
 		ResetLatch(MyLatch);
 
+#ifdef FAULT_INJECTOR
+		if (SIMPLE_FAULT_INJECTOR("ckpt_loop_begin") == FaultInjectorTypeInfiniteLoop)
+			do_checkpoint = true;
+#endif 
+
 		/*
 		 * Process any requests or signals received recently.
 		 */
@@ -510,6 +516,8 @@ CheckpointerMain(void)
 
 		/* Send WAL statistics to the stats collector. */
 		pgstat_send_wal(true);
+
+		SIMPLE_FAULT_INJECTOR("ckpt_loop_end");
 
 		/*
 		 * If any checkpoint flags have been set, redo the loop to handle the
