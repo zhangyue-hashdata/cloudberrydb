2e922468d61096f3477a279f71439494b79c1b62
dreamedcheng <drec.wu@gmail.com>
2023-02-23 08:48:46 +0800

    Fix LWLockHeldByMe assert failure in SharedSnapshotDump (#15007)
    Note that when SharedSnapshotDump is called, the caller must hold the
    SharedSnapshotLock in advance, so this commit fixes the case that didn't
    satisfy this condition.
    
    Co-authored-by: wuchengwen <wcw190496@alibaba-inc.com>

--- src/backend/utils/time/sharedsnapshot.c
+++ src/backend/utils/time/sharedsnapshot.c
@@ -368,8 +368,9 @@ retry:
 
 		if (testSlot->slotindex > arrayP->maxSlots)
 		{
+			char *slot_dump = SharedSnapshotDump();
 			LWLockRelease(SharedSnapshotLock);
-			elog(ERROR, "Shared Local Snapshots Array appears corrupted: %s", SharedSnapshotDump());
+			elog(ERROR, "Shared Local Snapshots Array appears corrupted: %s", slot_dump);
 		}
 
 		if (testSlot->slotid == slotId)
