a43da9bb2ce88f2fa12b47786cb0dc26356bbd5b
Huansong Fu <fuhuansong@gmail.com>
2023-04-05 08:38:11 -0700

    Fix possible inconsistency between bitmap LOV table and index
    Similar to gp_fastsequence, there was a potential inconsistency between
    the bitmap LOV table and its index due to the frozen insert. Now we fix
    the inconsistency by similar method as in 961de2da40858ed302b2a656b5258aa8d17c87b9

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -5794,6 +5794,7 @@ PostgresMain(int argc, char *argv[],
 
 					SetUserIdAndSecContext(GetOuterUserId(), 0);
 
+					SIMPLE_FAULT_INJECTOR("qe_exec_finished");
 					send_ready_for_query = true;
 				}
 				break;
