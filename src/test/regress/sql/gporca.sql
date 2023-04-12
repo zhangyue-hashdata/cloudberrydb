33b9cdd6878fa43aedca9ce33a149700dd890d74
Ekta Khanna <ekhanna@vmware.com>
2023-04-11 17:45:22 -0700

    Update MDP tests and add test to ICG

--- src/test/regress/sql/gporca.sql
+++ src/test/regress/sql/gporca.sql
@@ -3575,6 +3575,13 @@ insert into ts_tbl select to_timestamp('99991231'::text, 'YYYYMMDD'::text) from 
 analyze ts_tbl;
 explain select * from ts_tbl where ts = to_timestamp('99991231'::text, 'YYYYMMDD'::text);
 
+-- Test ORCA support for implicit array coerce cast
+create table array_coerce_foo (a int, b varchar(2)[]);
+create table array_coerce_bar (a int, b varchar(10)[]);
+
+insert into array_coerce_bar values (1, ARRAY['abcde']);
+explain insert into array_coerce_foo select * from array_coerce_bar;
+insert into array_coerce_foo select * from array_coerce_bar;
 
 WITH conf AS (
     SELECT setting
