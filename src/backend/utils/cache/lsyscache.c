324f0470d57a4e1c50816af622f1ad82708d0f18
Hari krishna <49222145+pobbatihari@users.noreply.github.com>
2023-06-30 14:15:26 +0530

    Fix for supporting deferrable keyword on primary/unique keys (#15816)
    * Fix for supporting deferrable keyword on primary/unique keys
    
    Issue:
          FIXME: When there is a deferrable keyword on the Primary/unique key, it cannot be opimized but orca does it.
    
    create table foo(a int, b int, c int, primary key(a, b) deferrable);
    select * from foo group by a, b, c;
              QUERY PLAN
    ------------------------------------------------------
     Gather Motion 3:1  (slice1; segments: 3)
       ->  Seq Scan on foo
     Optimizer: Pivotal Optimizer (GPORCA)
    
    RCA:
    During the process of retrieving Primary/unique keySets from the catalog
    (pg_constraint), Orca does not perform any deferrable checks. As a result,
    the keySets will include all the keys, including those that are
    deferrable. This is the reason why CXformSimplifyGbAgg transforms GbAgg
    to Select when the grouping column includes a key.
    
    To address this issue, a deferral check has been added at the
    get_relation_keys() function when retrieving KeySets. This check ensures
    that only non-deferrable keys are included in the keySets, allowing for
    more accurate transformations and optimizations.
    
    What is deferrable Keyword?
    In PostgreSQL, the condeferrable and condeferred columns in the
    pg_constraint system catalog table store information about the
    deferrability and initial deferral state of constraints.
    
    condeferrable: This column indicates whether a constraint is deferrable
    or not. If the value is true, it means the constraint is deferrable. If
    the value is false, the constraint is not deferrable.
    
    condeferred: This column indicates the initial deferral state of a
    constraint. If the value is true,  it means the constraint is initially
    deferred, meaning the constraint checking is deferred until the end of
    the transaction. If the value is false, the constraint is initially
    immediate, meaning the constraint checking is performed immediately
    after each statement within the transaction.
    
    By default, primary key constraints in PostgreSQL are not deferrable,
    which means condeferrable is set to false. This means that primary key
    constraints are always checked immediately after each statement
    (condeferred is false). If you explicitly specify the DEFERRABLE keyword
    when defining a primary key constraint, condeferrable will be set to
    true, indicating that the primary key constraint is deferrable. The
    INITIALLY DEFERRED clause can also be used to set condeferred to true,
    indicating that the primary key constraint is initially deferred.
    It's important to note that the behavior of primary key constraints
    depends on the values of condeferrable and condeferred. If both columns
    are false, the primary key constraint is not deferrable and is always
    checked immediately. If condeferrable is true and condeferred is true,
    the primary key constraint is deferrable and initially deferred.
    
    After Fix:
    
    explain select * from foo group by a, b, c;
                                      QUERY PLAN
    -------------------------------------------------------------------------------
     Gather Motion 3:1  (slice1; segments: 3)  (cost=0.00..431.00 rows=1 width=12)
       ->  GroupAggregate  (cost=0.00..431.00 rows=1 width=12)
             Group Key: a, b, c
             ->  Sort  (cost=0.00..431.00 rows=1 width=12)
                   Sort Key: a, b, c
                   ->  Seq Scan on foo  (cost=0.00..431.00 rows=1 width=12)
     Optimizer: Pivotal Optimizer (GPORCA)

--- src/backend/utils/cache/lsyscache.c
+++ src/backend/utils/cache/lsyscache.c
@@ -4141,7 +4141,13 @@ get_relation_keys(Oid relid)
 		{
 			continue;
 		}
-			
+
+		// skip the constraint if deferrable
+		if (contuple->condeferrable)
+		{
+			continue;
+		}
+
 		// store key set in an array
 		List *key = NIL;
 		
