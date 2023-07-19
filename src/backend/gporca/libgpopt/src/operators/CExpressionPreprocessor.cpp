4b6211cb97ed92d41cf80764b49c262446db5d57
Chris Hajas <chajas@vmware.com>
2023-07-19 10:44:54 -0700

    Fix memory leak in Orca (#16006)
    We're adding an expression to a hashmap, but if the element already
    exists it won't be added but the refcount would still be incremented.

--- src/backend/gporca/libgpopt/src/operators/CExpressionPreprocessor.cpp
+++ src/backend/gporca/libgpopt/src/operators/CExpressionPreprocessor.cpp
@@ -1994,9 +1994,13 @@ UpdateExprToConstantPredicateMapping(CMemoryPool *mp, CExpression *pexprFilter,
 		{
 			if (doInsert)
 			{
-				(*pexprFilter)[0]->AddRef();
-				(*pexprFilter)[1]->AddRef();
-				phmExprToConst->Insert((*pexprFilter)[0], (*pexprFilter)[1]);
+				BOOL inserted = phmExprToConst->Insert((*pexprFilter)[0],
+													   (*pexprFilter)[1]);
+				if (inserted)
+				{
+					(*pexprFilter)[0]->AddRef();
+					(*pexprFilter)[1]->AddRef();
+				}
 			}
 			else
 			{
@@ -2009,9 +2013,13 @@ UpdateExprToConstantPredicateMapping(CMemoryPool *mp, CExpression *pexprFilter,
 		{
 			if (doInsert)
 			{
-				(*pexprFilter)[0]->AddRef();
-				(*pexprFilter)[1]->AddRef();
-				phmExprToConst->Insert((*pexprFilter)[1], (*pexprFilter)[0]);
+				BOOL inserted = phmExprToConst->Insert((*pexprFilter)[1],
+													   (*pexprFilter)[0]);
+				if (inserted)
+				{
+					(*pexprFilter)[0]->AddRef();
+					(*pexprFilter)[1]->AddRef();
+				}
 			}
 			else
 			{
