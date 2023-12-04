8d351ab99feb1dfb0c432e22c40517e1758659f5
HelloYJohn <xxdrywx@gmail.com>
2023-12-04 16:19:37 +0800

    fix alter table alter column type reuse index error
    Update alter_table.out
    
    Update alter_table.sql
    
    fix comment and test

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -15053,8 +15053,9 @@ ATPostAlterTypeParse(Oid oldId, Oid oldRelId, Oid refRelId, char *cmd,
 				irel = index_open(indoid, AccessShareLock);
 			}
 
-			/* replace it with my own */
-			stmt->oldNode = irel->rd_node.relNode;
+			/* If it is for the current index, replace the relnode with my own. */
+			if (strcmp(stmt->idxname, irel->rd_rel->relname.data) == 0)
+				stmt->oldNode = irel->rd_node.relNode;
 		}
 
 		if (irel != NULL)
