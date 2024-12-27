679a1e26ac75e6c839af144db2f7a0fb1ea1c558
Jianghua.yjh <yjhjstz@gmail.com>
2024-12-27 11:07:46 +0800

    Fix flake test bfv_meta_track (#817)
    One suppose is that oid_wraparound test is before bfv_meta_track test.
    Add stasubtype limit , make test stable.

--- src/test/regress/sql/bfv_meta_track.sql
+++ src/test/regress/sql/bfv_meta_track.sql
@@ -8,30 +8,31 @@ drop schema if exists bfv_meta_track;
 -- end_ignore
 create schema bfv_meta_track;
 set search_path to bfv_meta_track;
-select staactionname, stasubtype, nspname from pg_stat_last_operation join
-  pg_namespace on pg_namespace.oid = pg_stat_last_operation.objid
-  where pg_namespace.nspname = 'bfv_meta_track';
+select count(*) from pg_stat_last_operation p join
+  pg_namespace on pg_namespace.oid = p.objid
+  where pg_namespace.nspname = 'bfv_meta_track' and p.stasubtype = 'SCHEMA';
 
 -- test drop popicy
 create table t1(a int);
 create policy p1 on t1 using (a % 2 = 0);
-select staactionname, stasubtype from pg_stat_last_operation a join pg_policy b on b.oid = a.objid where b.polname = 'p1' and b.polrelid ='t1'::regclass::oid;
--- start_ignore
-select a.xmin as pg_stat_last_operation_xmin, a.xmax as pg_stat_last_operation_xmax,
-  b.xmin as pg_policy_xmin, b.xmax as pg_policy_xmax,
-  staactionname, stasubtype from pg_stat_last_operation a join pg_policy b on b.oid = a.objid
-  where b.polname = 'p1' and b.polrelid ='t1'::regclass::oid;
-select xmin, xmax, relname from pg_class where oid in (select objid from (select objid, staactionname, stasubtype from pg_stat_last_operation a join pg_policy b on b.oid = a.objid where b.polname = 'p1' and b.polrelid ='t1'::regclass::oid ) a) ;
--- end_ignore
-select relname from pg_class where oid in (select objid from (select objid, staactionname, stasubtype from pg_stat_last_operation a join pg_policy b on b.oid = a.objid where b.polname = 'p1' and b.polrelid ='t1'::regclass::oid ) a) ;
-
+select count(*) from pg_stat_last_operation a join pg_policy b on b.oid = a.objid
+where b.polname = 'p1' and b.polrelid ='t1'::regclass::oid and a.stasubtype = 'POLICY';
 drop policy p1 on t1;
-select staactionname, stasubtype from pg_stat_last_operation a join pg_policy b on b.oid = a.objid where b.polname = 'p1' and b.polrelid ='t1'::regclass::oid;
+select count(*) from pg_stat_last_operation a join pg_policy b on b.oid = a.objid
+where b.polname = 'p1' and b.polrelid ='t1'::regclass::oid and a.stasubtype = 'POLICY';
 
 --test drop publication
 -- start_ignore
 create publication pub1;
 -- end_ignore
-select staactionname, stasubtype from pg_stat_last_operation a join pg_publication b on b.oid = a.objid where b.pubname = 'pub1';
+select count(*) from pg_stat_last_operation a join pg_publication b on b.oid = a.objid
+where b.pubname = 'pub1' and a.stasubtype = 'PUBLICATION';
 drop publication pub1;
-select staactionname, stasubtype from pg_stat_last_operation a join pg_publication b on b.oid = a.objid where b.pubname = 'pub1';
+select count(*) from pg_stat_last_operation a join pg_publication b on b.oid = a.objid
+where b.pubname = 'pub1' and a.stasubtype = 'PUBLICATION';
+
+drop schema bfv_meta_track cascade;
+-- test drop schema
+select count(*) from pg_stat_last_operation join
+  pg_namespace on pg_namespace.oid = pg_stat_last_operation.objid
+  where pg_namespace.nspname = 'bfv_meta_track';
