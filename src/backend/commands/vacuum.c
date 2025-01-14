greenplum-db/gpdb-archive

Refactor AO macro RelationIsAppendOptimized (#15546)
This PR aims to refactor macro RelationIsAppendOptimized  which includes partition check in GP7.
Compared to GP6, now we didn't create auxiliary tables for the partition table because it doesn't
store any data in real, and that's the reason caused some bugs and it's also the motivation for refactoring it.
Removed partition check in `RelationIsAppendOptimized `, then consistent with GP6 in behavior,
add a new macro `RelationStorageIsAO` which stands for table store data in real and has auxiliary tables
except root partition table.
- For callsite of RelationStorageIsAO
if it's according to aux tables or storage, like most code related to storage and executor
- For callsite of RelationIsAppendOptimized
mainly concentrate on root partition table, like in parser and planner.

src/backend/commands/vacuum.c
@@ -1516,7 +1516,7 @@ vac_update_relstats(Relation relation,
 		{
 			Assert(Gp_role == GP_ROLE_UTILITY);
 			Assert(!IsSystemRelation(relation));
-			Assert(RelationIsAppendOptimized(relation));
+			Assert(RelationStorageIsAO(relation));
 			num_tuples = 0;
 		}
 
@@ -2285,7 +2285,7 @@ vacuum_rel(Oid relid, RangeVar *relation, VacuumParams *params,
 	else
 		toast_relid = InvalidOid;
 
-	if (RelationIsAppendOptimized(onerel))
+	if (RelationStorageIsAO(onerel))
 	{
 		/*
 		 * GPDB: AO tables should never be passed into vacuum_rel if the
@@ -2380,7 +2380,7 @@ vacuum_rel(Oid relid, RangeVar *relation, VacuumParams *params,
 		return false;
 	}
 
-	is_appendoptimized = RelationIsAppendOptimized(onerel);
+	is_appendoptimized = RelationStorageIsAO(onerel);
 	is_toast = (onerel->rd_rel->relkind == RELKIND_TOASTVALUE);
 
 	if (ao_vacuum_phase && !(is_appendoptimized || is_toast))

zhangyue-hashdata/cloudberrydb

src/backend/commands/vacuum.c
@@ -1701,6 +1701,7 @@ vac_update_relstats(Relation relation,
 		{
 			Assert(Gp_role == GP_ROLE_UTILITY);
 			Assert(!IsSystemRelation(relation));
+			Assert(RelationStorageIsAO(relation));
 			num_tuples = 0;
 		}
 
@@ -2514,7 +2515,7 @@ vacuum_rel(Oid relid, RangeVar *relation, VacuumParams *params,
 	else
 		toast_relid = InvalidOid;
 
-	if (RelationIsAppendOptimized(rel))
+	if (RelationStorageIsAO(rel))
 	{
 		/*
 		 * GPDB: AO tables should never be passed into vacuum_rel if the
@@ -2610,7 +2611,7 @@ vacuum_rel(Oid relid, RangeVar *relation, VacuumParams *params,
 		return false;
 	}
 
-	is_appendoptimized = RelationIsAppendOptimized(rel);
+	is_appendoptimized = RelationStorageIsAO(rel);
 	is_toast = (rel->rd_rel->relkind == RELKIND_TOASTVALUE);
 
 	if (ao_vacuum_phase && !(is_appendoptimized || is_toast))