c5f05d1276e3f1d6f0db65f93db61e24823f6fbb
Hao Wu <gfphoenix78@gmail.com>
2025-05-06 10:14:30 +0000

    PAX: Fix RMGR desc function for pax
    The variable filename is only used to store the filename, not for
    the relative path to datadir.
    
    It also free the memory allocated by the relpathbackend.

--- contrib/pax_storage/src/cpp/storage/wal/paxc_desc.c
+++ contrib/pax_storage/src/cpp/storage/wal/paxc_desc.c
@@ -31,6 +31,7 @@
 #include "storage/wal/paxc_desc.h"
 #endif
 #include "common/relpath.h"
+#include "utils/palloc.h"
 
 void pax_rmgr_desc(StringInfo buf, XLogReaderState *record) {
   char *rec = XLogRecGetData(record);
@@ -39,31 +40,28 @@ void pax_rmgr_desc(StringInfo buf, XLogReaderState *record) {
   switch (info) {
     case XLOG_PAX_INSERT: {
       char filename[MAX_PATH_FILE_NAME_LEN];
-      char *relpathPart;
-      size_t relpathPartSz;
+      char *relpath_part;
+      char *prefix;
+      int32 buffer_len;
 
       char *rec = XLogRecGetData(record);
       xl_pax_insert *xlrec = (xl_pax_insert *)rec;
 
       Assert(xlrec->target.file_name_len < MAX_PATH_FILE_NAME_LEN);
+      memcpy(filename, rec + SizeOfPAXInsert, xlrec->target.file_name_len);
+      filename[xlrec->target.file_name_len] = '\0';
 
-      relpathPart = relpathbackend(xlrec->target.node, InvalidBackendId, MAIN_FORKNUM);
-      relpathPartSz = strlen(relpathPart);
+      relpath_part = relpathbackend(xlrec->target.node, InvalidBackendId, MAIN_FORKNUM);
+      prefix = psprintf("%s_pax", relpath_part);
+      pfree(relpath_part);
 
-      memcpy(filename, relpathPart, relpathPartSz);
-
-#define PAX_DIR_SUFFIX "_pax/"
-
-      memcpy(filename + relpathPartSz, PAX_DIR_SUFFIX, strlen(PAX_DIR_SUFFIX));
-      memcpy(filename + relpathPartSz + strlen(PAX_DIR_SUFFIX), rec + SizeOfPAXInsert, xlrec->target.file_name_len);
-      filename[relpathPartSz + xlrec->target.file_name_len + strlen(PAX_DIR_SUFFIX)] = '\0';
-
-      int32 bufferLen = XLogRecGetDataLen(record) - SizeOfPAXInsert -
+      buffer_len = XLogRecGetDataLen(record) - SizeOfPAXInsert -
                         xlrec->target.file_name_len;
       appendStringInfo(buf,
-                       "PAX_INSERT, filename = %s, offset = %ld, "
+                       "PAX_INSERT, filename = %s/%s, offset = %ld, "
                        "dataLen = %d",
-                       filename, xlrec->target.offset, bufferLen);
+                       prefix, filename, xlrec->target.offset, buffer_len);
+      pfree(prefix);
       break;
     }
     case XLOG_PAX_CREATE_DIRECTORY: {
