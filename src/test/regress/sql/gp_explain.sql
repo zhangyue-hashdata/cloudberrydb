54087f70bf8c9b62e6ccef81ee1b7799cf8c233c
Dongxiao Song <songdongxiao@hashdata.cn>
2025-06-03 18:46:38 +0800

    Fix unstable scan rows caused by limit squelch.
    Squelch in limit node may cause scan finish early, row count in scan may vary.
    
    Signed-off-by: Dongxiao Song <songdongxiao@hashdata.cn>

--- src/test/regress/sql/gp_explain.sql
+++ src/test/regress/sql/gp_explain.sql
@@ -285,7 +285,10 @@ DROP USER regress_range_parted_user;
 -- Test if explain analyze will hang with materialize node
 CREATE TABLE recursive_table_ic (a INT) DISTRIBUTED BY (a);
 INSERT INTO recursive_table_ic SELECT * FROM generate_series(20, 30000);
-
+-- start_matchsubs
+-- m/Seq Scan on recursive_table_ic \(actual rows=\d+ loops=1\)/
+-- s/Seq Scan on recursive_table_ic \(actual rows=\d+ loops=1\)/Seq Scan on recursive_table_ic (actual rows=#### loops=1)/
+-- end_matchsubs
 explain (analyze, costs off, timing off, summary off) WITH RECURSIVE
 r(i) AS (
 	SELECT 1
