fed0458e0db36a5ce00d463d479ac1c156f488d0
Dianjin Wang <wangdianjin@gmail.com>
2025-07-29 17:21:42 +0800

    License: add & clean up license headers and files
    This commit performs a comprehensive license compliance cleanup to align
    with release requirements, which are pointed out by Incubator PMC
    review.
    
    The main changes include:
    
    1. Add License Headers: Added the standard Apache License Version 2.0
      header to numerous source files that were missing it. This covers
      multiple file types, including YAML, Markdown, SQL, C/C++, Python,
      and shell scripts. These files are originally created by the
      cloudberry community.
    2. Simplify LICENSE and NOTICE:
      - Restructured the root LICENSE file for better clarity.
      - Cleaned up the NOTICE file by removing redundant information which
        have been listed in the LICENSE.
    3. Remove the unused deployment docs from the `deploy/build`, which can
      help us manage the file licenses.
    4. Update RAT Configuration: Updated `pom.xml` to reflect the changes of
       the file license headers and attribution.
    
    See: https://github.com/apache/cloudberry/issues/1236

--- contrib/pax_storage/tools/gen_sql.c
+++ contrib/pax_storage/tools/gen_sql.c
@@ -1,12 +1,31 @@
-//
-// This program is used to generate sql script file for PAX.
-// Build and run of this program is automatically done in the
-// cmake script.
-//
-// The command to build this program alone is:
-// gcc -I`pg_config --includedir-server` -I<pax_dir>/src/cpp -o generate_sql gen_sql.c
-//
-//
+/*-------------------------------------------------------------------------
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ * This program is used to generate sql script file for PAX.
+ * Build and run of this program is automatically done in the
+ * cmake script.
+ *
+ * The command to build this program alone is:
+ * gcc -I`pg_config --includedir-server` -I<pax_dir>/src/cpp -o generate_sql gen_sql.c
+ *
+ *-------------------------------------------------------------------------
+ */
 
 #include "postgres.h"  // NOLINT
 
